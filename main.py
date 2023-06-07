486719e8bd2cba096528a1e4a79d6ab271275f15
IbrahimIsmayilov <i.ismayilov@share.epsb.ca>
2023-06-07 08:02:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Platformer Game Portfolio Project by Ibrahim Ismayilov
 
-# Import pygame module
+# IMPORTING MODULES 
+# Import pygame
 import pygame
 
+# INTIALIZE MODULES OR DIRECTORIES
 # Intialize pygame module
 pygame.init()
 
+# SET UP PYGAME WINDOW FOR DISPLAY 
 # Change pygame window title
 pygame.display.set_caption("Platformer Game by Ibrahim Ismayilov")
 
@@ -14,4 +17,18 @@ WIDTH, HEIGHT = 1500, 1200
 
 # Intialize the pygame window for display
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# GLOBAL VARIABLES TO BE ACCESSED LATER
+# Create FPS variable to set max FPS of the game to be 60 when run
+FPS = 60
+
+# FUNCTIONS
+# Main function to contain event handlers and run non-stop while program is open
+def main(window):
+    # A new clock variable that will be used to track the time and set a framerate
+    clock = pygame.time.Clock() 
+
+    # Constant, infinitely running loop
+    run = True
+
 
