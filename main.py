039d60ee65d1d0ad8f48b0628eaec14fcb2462e4
IbrahimIsmayilov <i.ismayilov@share.epsb.ca>
2023-06-07 08:01:41 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Platformer Game Portfolio Project by Ibrahim Ismayilov
+
+# Import pygame module
+import pygame
+
+# Intialize pygame module
+pygame.init()
+
+# Change pygame window title
+pygame.display.set_caption("Platformer Game by Ibrahim Ismayilov")
+
+# Width and height of pygame window stored in variables
+WIDTH, HEIGHT = 1500, 1200
+
+# Intialize the pygame window for display
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
